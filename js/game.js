924ee713a19461f08b97a08a783050c88196296c
Matthew Raghunauth <mar477@drexel.edu>
2016-06-28 14:49:33 -0400

    Week 02 changes
    Starting to build solo pong game.

--- js/game.js
+++ js/game.js
@@ -19,4 +19,46 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+//DO NOT TOUCH CODE ABOVE 
+
+//console.log('Holla');
+
+//STEP 1 .. MAR .. Create a game canvas and track mouse position 
+var gameCanvas = document.getElementById("canvas");
+// Store HTML5 canvas tag into a javascript variable 
+var ctx = gameCanvas.getContext("2d"); // Create context 2d
+var W = window.innerWidth;
+var H = window.innerHeight;
+var mouseobj = {};
+
+console.log('browswer width is currently: ' + W);
+console.log('browswer height is currently: ' + H);
+
+gameCanvas.width = W; 
+gameCanvas.height = H; 
+
+
+function paintCanvas(){
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, W, H);
+}
+paintCanvas();
+
+
+
+function trackPosition(evt) {
+    mouseobj.x = evt.pageX; 
+    mouseobj.y = evt.pageY; 
+    console.log("cursor x is :" + mouseobj.x + "cursor y is :" + mouseobj.y); 
+}
+gameCanvas.addEventListener("mousemove", trackPosition, true); 
+
+
+
+
+
+
+
+
+
+
