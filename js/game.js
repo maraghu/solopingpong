eaccd7c50d8e8b9cac8eb217e1995f5190a2d78c
Matthew Raghunauth <mar477@drexel.edu>
2016-07-12 12:39:20 -0400

    Adding sound and js
    have sound for game and update javascript code

--- js/game.js
+++ js/game.js
@@ -1,122 +1,113 @@
 // RequestAnimFrame: a browser API for getting smooth animations
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame   ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame    ||
-			window.oRequestAnimationFrame      ||
-			window.msRequestAnimationFrame     ||
-		function( callback ){
-			return window.setTimeout(callback, 1000 / 60);
-		};
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
 })();
 
-window.cancelRequestAnimFrame = ( function() {
-	return  window.cancelAnimationFrame          ||
-			window.webkitCancelRequestAnimationFrame ||
-			window.mozCancelRequestAnimationFrame    ||
-			window.oCancelRequestAnimationFrame      ||
-			window.msCancelRequestAnimationFrame     ||
-		clearTimeout;
-} )();
+window.cancelRequestAnimFrame = (function() {
+    return window.cancelAnimationFrame ||
+        window.webkitCancelRequestAnimationFrame ||
+        window.mozCancelRequestAnimationFrame ||
+        window.oCancelRequestAnimationFrame ||
+        window.msCancelRequestAnimationFrame ||
+        clearTimeout;
+})();
 
-//DO NOT TOUCH CODE ABOVE 
+// DO NOT TOUCH CODE ABOVE
 
-//console.log('Holla');
 
-//STEP 1 .. MAR .. Create a game canvas and track mouse position 
-var gameCanvas = document.getElementById("canvas");
-// Store HTML5 canvas tag into a javascript variable 
-var ctx = gameCanvas.getContext("2d"); // Create context 2d
+// Step 01 .. jwt .. Create game canvas and track mouse position
+var gameCanvas = document.getElementById('canvas'); // Store HTML5 canvas tag into a JS variable
+
+var ctx = gameCanvas.getContext('2d'); // Create context 2D
+
 var W = window.innerWidth;
 var H = window.innerHeight;
-var mouseobj = {};
+var mouseObj = {};
 
-console.log('browswer width is currently: ' + W);
-console.log('browswer height is currently: ' + H);
+// console.log('browser width is currently:' + W);
+// console.log('browser height is currently:' + H);
 
-gameCanvas.width = W; 
-gameCanvas.height = H; 
+gameCanvas.width = W;
+gameCanvas.height = H;
 
 
-//step 02 . . mr. . clear page canvas by covering it in black 
+// Step 02 .. jwt .. Clear page canvas by covering it in black.
 
-function paintCanvas(){
-    ctx.fillStyle = "#000000";
+function paintCanvas() {
+    ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, W, H);
 }
+
 paintCanvas();
 
-
-
 function trackPosition(evt) {
-    mouseobj.x = evt.pageX; 
-    mouseobj.y = evt.pageY; 
-    console.log("cursor x is :" + mouseobj.x + "cursor y is :" + mouseobj.y); 
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    // console.log("cursor x is : " + mouseObj.x + "cursor y is : " + mouseObj.y);
 }
-gameCanvas.addEventListener("mousemove", trackPosition, true); 
 
-//step 03  . . mr. . place a ball on the canvas 
+gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+
+// Step 03 .. jwt .. Place a ball on the canvas
 var ball = {}; // Ball Object
-ball ={
-    
+ball = {
     x: 50,
     y: 50,
     r: 5,
-    c: "#ffffff",
-    vs: 4,
+    c: "#fff",
+    vx: 4,
     vy: 8,
-    
-    
-draw: function() {
-    
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-    ctx.fill();
-}   
+    draw: function() {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
 }
+
 ball.draw();
 
-//Step 04  . . mr . . place a start button on canvas 
 
-var startBtn = {}; // Start button object 
+// Step 04 .. jwt .. Place a start button on canvas
+var startBtn = {}; // Start button object
 startBtn = {
-    
     w: 100,
     h: 50,
     x: W / 2 - 50,
     y: H / 2 - 25,
-    
-    draw: function () {
-        
-        ctx.strokeStyle= "#fff";
-        ctx.lineWidth= "2";
-        ctx.strokeRect= (this.x, this.y, this.w, this.h);
-        
+
+    draw: function() {
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+
         ctx.font = "18px Arial, sans-serif";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.fillStyle = "#fff";
-        ctx.fillText = ("Start, W/2, H/2");
+        ctx.fillText("START", W / 2, H / 2);
     }
 }
 
 startBtn.draw();
 
-
-//step 05 . . mr . . place score and points on canvas
-
+// Step 05 .. jwt .. Place score and points on canvas
 var points = 0; // game points
 function paintScore() {
-    
     ctx.fillStyle = "#fff";
     ctx.font = "18px Arial, sans-serif";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
-    ctx.fillText = ("Score: " + points, 20, 20);
-    
-    
-    
+    ctx.fillText("SCORE: " + points, 20, 20);
+
 }
 
 paintScore();
